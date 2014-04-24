ed12b22f30760123555cacdf0e44daaf765eb16c
HappySmilyGuy <crazy-ginger@hotmail.co.uk>
2014-04-24 15:39:23 +0100

    Incorporated presets with main file

--- MIDI_in_thu/MIDI_in_thu.ino
+++ MIDI_in_thu/MIDI_in_thu.ino
@@ -1,21 +1,35 @@
 #include <MIDI.h>
+#include <EEPROM.h>
 
-// --------- Control constants --------- //
-const boolean DEBUG = false; // false to remove all printouts and LED flashes
-const int STEPDELAY = 200;   // delay between each step in micro seconds
-const float SPR = 1600.0;    // steps per complete rotation of the stepper motor
-const int CW = LOW;          // clockwize, for DIR pin
-const int ACW = HIGH;        // anticlockwise, for DIR pin
+
+// --------- Control definitions and constants --------- //
+// General
+#define DEBUG false     // false to remove all printouts and LED flashes
+// Motor definitions
+#define STEPDELAY 200   // delay between each step in micro seconds
+#define SPR 1600.0      // steps per complete rotation of the stepper motor
+#define CW LOW          // clockwize, for DIR pin
+#define ACW HIGH        // anticlockwise, for DIR pin
+// Preset definitions
+#define MAX_PRESETS 128
+#define MAX_MOTORS 16
+#define ENCODER_TO_MOTOR_RATIO 80 //1600/20
+#define NOON 127        // dial pointing straight up, neutral position
 
 // --------- PINS --------- //
-const int DIR = 53; // Easy driver direction pin
-const int STEP = 2; // Easy driver step pin
-const int SLP = 52; // Easy Driver sleep pin
-const int LED = 13; // Arduino LED (L) control
-const int CLEAR_ALL = 39; // reset button press
+// General
+#define LED 13          // Arduino LED (L) control
+#define CLEAR_ALL 39    // reset button press
+// Motors
+#define DIR 53          // Easy driver direction pin
+#define STEP 2          // Easy driver step pin
+#define SLP 52          // Easy Driver sleep pin
+
 
 // --------- Global Variables --------- //
-
+int presets[MAX_MOTORS][MAX_PRESETS];
+int currentPositions[MAX_MOTORS];
+int currentPreset;
 
 
 
@@ -24,9 +38,8 @@ void setup(){
   
   // MIDI initialising
   MIDI.begin(MIDI_CHANNEL_OMNI);  // listen for MIDI on all channels at all frequencies
-  MIDI.setHandleControlChange(ControlChanges);
   MIDI.setHandleProgramChange(ChangePreset);
-  MIDI.turnThruOn();
+  // MIDI.turnThruOn();
   
   // Stepper Motor Driver Initialistion
   pinMode(DIR, OUTPUT);     
@@ -35,28 +48,19 @@ void setup(){
   pinMode(CLEAR_ALL, INPUT);
   digitalWrite(DIR, HIGH);
   digitalWrite(STEP, LOW);
-  digitalWrite(SLP, HIGH); 
+  digitalWrite(SLP, HIGH);
+
+  // Presets initialisation
+  loadPresets();
+  currentPreset = -1; // placeholder for not yet set.
 }
 
 void loop(){
   MIDI.read();
-  if(digitalRead(CLEAR_ALL) == HIGH){ // lots of delayed checks so it doesn't go off accidentily
-    delay(2000);
-    if(digitalRead(CLEAR_ALL) == HIGH){
-      delay(2000);
-      if(digitalRead(CLEAR_ALL) == HIGH){
-        delay(2000);
-        if(digitalRead(CLEAR_ALL) == HIGH){
-          digitalWrite(LED, HIGH);
-          delay(1000);
-          digitalWrite(LED,LOW);
-        }
-      }
-    }
-  }
+  checkClearMemoryButton();
 }
 
-
+// --------- MIDI FUNCTIONS --------- //
 void ChangePreset(byte channel, byte number) {
   //pedal footswitches use channel 0
   //channel is generally unused.
@@ -69,7 +73,7 @@ void ChangePreset(byte channel, byte number) {
   
   
   
-  // ----- Debugging ----- //
+  // ----- Debugging
   if(DEBUG){    
     for(int i = 0; i < (int)number; i++){ //flashes main LED the same amount of times as the number of the preset that was just called.
       digitalWrite(LED, HIGH);
@@ -77,53 +81,12 @@ void ChangePreset(byte channel, byte number) {
       digitalWrite(LED,LOW);
       delay(500);
     }
-    Serial.print("\nCHANNEL\ndec: ");
-    Serial.println((int)channel, DEC);
-    Serial.print("hex: ");
-    Serial.println(channel, HEX);
-    Serial.print("bin: ");
-    Serial.println(channel, BIN);
-    Serial.flush();
-    Serial.print("\nNUMBER\ndec: ");
-    Serial.println((int)number, DEC);
-    Serial.print("hex: ");
-    Serial.println(number, HEX);
-    Serial.print("bin: ");
-    Serial.println(number, BIN);
-    Serial.flush();
   }
   
 }
 
-void ControlChanges(byte channel, byte number, byte value){
-  if(DEBUG){
-    Serial.print("\nControl Change:\nchannel: ");
-    Serial.println((int)channel, DEC);
-    Serial.print("\nNote: ");
-    Serial.println((int)number, DEC);
-    Serial.print("value: ");
-    Serial.println((int)value, DEC);
-    Serial.flush();
-  }
-  // 0: bank select 
-  // 1: mod wheel 
-  // 2: breath controller 
-  // 5: portamento time 
-  // 6: data entry -- used with the registered parameter mechanism 
-  // 7: volume 
-  // 10: pan -- adjusts the left-right positioning of a signal in a stereo output 
-  // 11: expression pedal 
-  // 64: sustain pedal 
-  // 65: portamento on/off 
-  // 88: High resolution velocity prefix (added 2010) 
-  // 98 & 99: non-registered parameter number 
-  // 100 & 101: registered parameter number 
-  // 121: reset all controllers 
-  // 122: local control on/off 
-  // 123: all notes off 
-}
-
-void DriveMotor(int angle, int dir){  
+// --------- MOTOR FUNCATIONS ------- //
+void DriveMotor(int motor, int angle, int dir){  
   digitalWrite(SLP, HIGH);
   float rotate = (float) (angle * SPR) / 360.0; // to avoid rounding error
   for(int i = 0; i < rotate; i++){
@@ -133,5 +96,74 @@ void DriveMotor(int angle, int dir){
       delayMicroseconds(STEPDELAY);
   }
   digitalWrite(SLP, LOW); 
+}
+
+
+// --------- Preset Funcations ---------- //
+inline void updateCurrentPosition(int motor, int newPosition){
+  currentPositions[motor] = newPosition;
+  EEPROM.write(currentLocationMemoryLocation(motor), newPosition);
+}
+
+inline int recallPreset(int motor, int preset){
+  return presets[motor][preset];
+}
+
+void savePreset(int motor, int currentPosition){
+  if(currentPreset != -1){
+    presets[motor][currentPreset] = currentPosition;
+    EEPROM.write(presetMemoryLocation(motor, currentPosition), currentPosition);
+  }
+  updateCurrentPosition(motor, currentPosition);
+}
+
+int moveToPreset(int motor, int preset){
+  int pos = recallPreset(motor, preset);
+  if(pos != 255){
+    pos = pos - currentPositions[motor];
+    if(pos < 0) DriveMotor(motor, pos, CW);
+    else if(pos > 0) DriveMotor(motor, pos, ACW);
+    updateCurrentPosition(motor, pos);
+  }
+  currentPreset = preset;
+}
+
+void loadPresets(){
+  for(int motor = 0; motor < MAX_MOTORS; motor++){
+    for(int preset = 0; preset < MAX_PRESETS + 1; preset++){
+      presets[motor][preset] = EEPROM.read(presetMemoryLocation(motor, preset));
+    }
+    currentPositions[motor] = EEPROM.read(currentLocationMemoryLocation(motor));
+  }
+}
+
+void clearMemory(){
+  for(int motor = 0; motor < MAX_MOTORS; motor++){
+    for(int preset = 0; preset < MAX_PRESETS; preset++){
+      EEPROM.write(presetMemoryLocation(motor, preset), 255);
+      presets[motor][preset] = 255;
+    }
+  }
+}
+
+void clearMotor(int motor){ // remove all presets and reset currentPosition for specific motor
+  resetCurrentPostion(motor);
+  for(int preset = 0; preset < MAX_PRESETS; preset++){
+    EEPROM.write(presetMemoryLocation(motor, preset), 255);
+    presets[motor][preset] = 255;
+  }
+}
+
+void resetCurrentPosition(int motor){
+  currentPosition[motor] = NOON;
+  EEPROM.write(currentLocationMemoryLocation(motor), NOON);
+}
+
+inline int presetMemoryLocation(int motor, int preset){
+  return motor * (MAX_PRESETS + 1) + preset + 1;
+}
+
+inline int currentLocationMemoryLocation(int motor){
+ return motor * (MAX_PRESETS + 1); 
 }
 
